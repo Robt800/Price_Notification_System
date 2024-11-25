d5f20d1feaa053c921790e19457b673b3e9e81fd
Robt800 <53785367+Robt800@users.noreply.github.com>
2024-11-25 19:59:56 +0000

    Removal of buffers and tidy up of code

--- main.go
+++ main.go
@@ -12,11 +12,9 @@ func main() {
 	Objects := []string{"Iron Man Figure", "Hulk Figure", "Deadpool Figure", "Wolverine Figure", "Spider-Man Figure",
 		"Thor Figure", "Superman Figure", "Batman Figure", "Wonder-Woman Figure", "Captain America Figure"}
 
-	//Create buffer for objects that have been traded
-	ObjectsBuffer := make([]string, len(Objects))
-
-	var individualTrades, allTrades []byte //Create variables that will hold the individual trades and all trades together
+	//Create variables that will hold the individual trades and all trades together
 	//as a slice of byte - which JSON format uses to store data
+	var individualTrades, allTrades []byte
 
 	//Create a channel of single bool - used to trigger trades
 	TriggerChannel := make(chan bool)
@@ -24,9 +22,12 @@ func main() {
 	//Generate a 'true' onto the channel randomly between 1-5 seconds - i.e. send to the channel
 	go tradeTrigger(TriggerChannel)
 
-	//use range function to trigger call trade function
+	//use range function to trigger call trade function.
+	//When the trade has been completed - print the trade out and
+	//add the trade to the 'allTrades' slice that holds 'all trades'
 	for _ = range TriggerChannel {
-		Objects, ObjectsBuffer, individualTrades = Trades.Trade(Objects, ObjectsBuffer)
+		individualTrades = Trades.Trade(Objects)
+		fmt.Printf("%v\n", string(individualTrades))
 		allTrades = append(allTrades, individualTrades...)
 	}
 
@@ -34,8 +35,10 @@ func main() {
 
 }
 
+// Function that triggers a set amount of trades (equal to i max value).
+// Trades are triggered 'randomly' between 1 and 5 second intervals.
 func tradeTrigger(trigger chan<- bool) {
-	for i := 0; i < 11; i++ {
+	for i := 0; i < 20; i++ {
 		randomSecs := int((rand.Float64() * 4.0) + 1)
 		time.Sleep(time.Duration(randomSecs) * time.Second)
 		trigger <- true
