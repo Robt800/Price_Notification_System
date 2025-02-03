6b6b91db65758175d5b4d444c21b596aa25bed90
Robt800 <53785367+Robt800@users.noreply.github.com>
2025-02-03 19:27:35 +0000

    Version for Anish to review.  The previous comments have been worked through. The main changes have focused on the unit testing of the trades package

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Price_Notification_System
 
 go 1.22
 
-require golang.org/x/sync v0.10.0
+require (
+	github.com/google/go-cmp v0.6.0
+	golang.org/x/sync v0.10.0
+)
--- trades/trades_test.go
+++ trades/trades_test.go
@@ -2,191 +2,75 @@ package trades
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"math"
+	"encoding/json"
+	"github.com/google/go-cmp/cmp"
+	"math/rand"
 	"slices"
 	"testing"
 	"time"
 )
 
 func TestTrade(t *testing.T) {
+	// set random seed for reproducibility
+	// this ensures that on every run, rand.Intn will produce the same sequence of numbers
+	rand.New(rand.NewSource(1))
 
-	type myTestDef struct {
-		param1  []string
-		param2	func() time.Time
-		want	string
+	type testDef struct {
+		testInstanceName string
+		tradeObjects     []string
+		timeProvider     func() time.Time
+		expected         TradeItems
+		wantErr          bool
 	}
 
-	var tests = []myTestDef{
-		myTestDef{param1: []string{"Laptop", "PC", "Monitor", "Keyboard", "Mouse"}, param2: func() time.Time { return time.Date(2024, time.January, 30, 16, 30, 0, 0, time.UTC) }},
-		myTestDef{param1: []string{"Table", "Chair", "Plate", "Knife", "Fork"}, param2: func() time.Time { return time.Date(2025, time.February, 25, 8, 15, 59, 0, time.UTC) }},
-		myTestDef{param1: []string{"Corvette", "Ford", "Pontiac", "Dodge", "Cadillac"}, param2: func() time.Time { return time.Date(2023, time.November, 05, 19, 29, 3, 0, time.UTC) } },
+	var tests = []testDef{
+		testDef{testInstanceName: "successful trade1", tradeObjects: []string{"Laptop", "PC", "Monitor", "Keyboard", "Mouse"},
+			timeProvider: func() time.Time { return time.Date(2024, time.January, 30, 16, 30, 0, 0, time.UTC) },
+			expected:     TradeItems{Object: "Monitor", Timestamp: time.Date(2024, time.January, 30, 16, 30, 0, 0, time.UTC), Price: 950},
+			wantErr:      false},
+		testDef{testInstanceName: "successful trade2", tradeObjects: []string{"Table", "Chair", "Plate", "Knife", "Fork"},
+			timeProvider: func() time.Time { return time.Date(2025, time.February, 25, 8, 15, 59, 0, time.UTC) },
+			expected:     TradeItems{Object: "Chair", Timestamp: time.Date(2025, time.February, 25, 8, 15, 59, 0, time.UTC), Price: 980},
+			wantErr:      false},
+		testDef{testInstanceName: "successful trade3", tradeObjects: []string{"Corvette", "Ford", "Pontiac", "Dodge", "Cadillac"},
+			timeProvider: func() time.Time { return time.Date(2023, time.November, 05, 19, 29, 3, 0, time.UTC) },
+			expected:     TradeItems{Object: "Pontiac", Timestamp: time.Date(2023, time.November, 05, 19, 29, 3, 0, time.UTC), Price: 890},
+			wantErr:      false},
 	}
 
-//	type returnedData struct {
-//		fullTrade  string
-//		item       string
-//		dateStamp  string
-//		price      string
-//		priceAsInt int
-//	}
+	// Iterate through the tests
+	for _, tt := range tests {
+		t.Run(tt.testInstanceName, func(t *testing.T) {
 
-//	var returnedDataSets = make([]returnedData, 3)
+			ctx := context.Background()
+			resultChannel := make(chan []byte, 1)
+			defer close(resultChannel)
 
+			err := tradeImpl(ctx, tt.tradeObjects, resultChannel, tt.timeProvider)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("tradeImpl() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 
-	//for i := range tests {
-	//	defer close(t.individualTrades)
-	//	err := Trade(i.ctx, tt.tradeObjects, tt.individualTrades)
-	//	if err != nil {
-	//		t.Error(err)
-	//	}
-	//}(&tests[i])
-	//}
+			if !tt.wantErr {
+				select {
+				case trade := <-resultChannel:
+					var tradedItem TradeItems
 
-
-	// Determine how to validate 1 test
-	want :=
-	ctx := context.Background()
-	resultChannel := make(chan []byte)
-	got := make([]byte,10,20)
-	err := tradeImpl(ctx, tests[0].param1, resultChannel, tests[0].param2)
-	if err != nil {
-		t.Error(err)
+					if err := json.Unmarshal(trade, &tradedItem); err != nil {
+						t.Errorf("Failed to unmarshal trade: %v", err)
+					}
+					if validObject := slices.Contains(tt.tradeObjects, tradedItem.Object); !validObject {
+						t.Errorf("The object %v is not included within the allowed trade items %v", tradedItem.Object, tt.tradeObjects)
+					}
+					if timeDiff := cmp.Diff(tt.expected.Timestamp, tradedItem.Timestamp); timeDiff != "" {
+						t.Errorf("The timestamp of the trade: %v does not match the anticipated timestamp: %v", tradedItem.Timestamp, tt.expected.Timestamp)
+					}
+					if priceWithinTolerance := (tradedItem.Price >= 800) && (tradedItem.Price <= 1050); !priceWithinTolerance {
+						t.Errorf("The traded price of %v is outside the permissable range of 800 - 1050", tradedItem.Price)
+					}
+				}
+			}
+		})
 	}
-	got = <-resultChannel
-
-	//Check to see see if we got what was expected
-	if got !=
-
-
-
-
-	//Get elements from the trade
-	fmt.Println("The returned data from the function is as follows:")
-	for i, test := range tests {
-		for trade := range test.individualTrades {
-			fmt.Printf("%s\n", trade)
-			returnedDataSets[i].item = obtainObjectTraded(string(trade))
-			returnedDataSets[i].dateStamp = obtainTimestampTrade(string(trade))
-			returnedDataSets[i].price = obtainPriceTrade(string(trade))
-		}
-	}
-
-	//Test to see if the object traded is valid
-	for i, data := range returnedDataSets {
-		if !slices.Contains(tests[i].tradeObjects, data.item) {
-			t.Error("Item not found in tradedObjects, expected something from:", tests[i].tradeObjects,
-				"got:", data.item)
-		}
-	}
-
-	//Test to see if the timestamp looks reasonable
-	timeStampOK, timeStampOfTrade, err := testTimeStampOK()
-	if err != nil {
-		t.Error(err)
-	}
-	if !timeStampOK {
-		t.Error("The timestamp of the trade is not within tolerance. i.e. ", timeStampOfTrade)
-	}
-
-	//Test to see if the price looks reasonable
-	for i := 0; i < len(returnedDataSets); i++ {
-		returnedDataSets[i].priceAsInt, err = strToInt(returnedDataSets[i].price)
-		if err != nil {
-			t.Error(err)
-		}
-		if returnedDataSets[i].priceAsInt < 800 ||
-			returnedDataSets[i].priceAsInt > 1050 {
-			t.Error("The returned price is outside the expected of £8 to £10.50")
-		}
-	}
-
-}
-
-func obtainObjectTraded(fullTrade string) string {
-	startPos := 11
-	endPos := elementTradedDoubleQuotesPos(fullTrade, 4)
-	return fullTrade[startPos:endPos]
-}
-
-func elementTradedDoubleQuotesPos(fullTrade string, doubleQuoteCountPosReq int) int {
-	noDoubleQuotesFound := 0
-	i := 0
-	var c rune = 0
-	for i, c = range fullTrade {
-		if c == rune('"') {
-			noDoubleQuotesFound++
-		}
-		if noDoubleQuotesFound == doubleQuoteCountPosReq {
-			break
-		}
-	}
-	return i
-}
-
-func obtainTimestampTrade(fullTrade string) string {
-	startPos := elementTradedDoubleQuotesPos(fullTrade, 7) + 1
-	endPos := elementTradedDoubleQuotesPos(fullTrade, 8)
-	return fullTrade[startPos:endPos]
-}
-
-func obtainPriceTrade(fullTrade string) string {
-	startPos := elementTradedDoubleQuotesPos(fullTrade, 10) + 2
-	endPos := len(fullTrade) - 1
-	return fullTrade[startPos:endPos]
-}
-
-func getTimeStampFromString(date string) (time.Time, error) {
-	const layout = "2006-01-02T15:04:05"
-
-	dateTime, err := time.Parse(layout, date[:len(date)-1])
-	return dateTime, err
-}
-
-func testTimeStampOK() (bool, time.Time, error) {
-	var timeStampOK bool
-	var timeStampOfTrade time.Time
-	for _, data := range returnedDataSets {
-		timestampOfTrade, err := getTimeStampFromString(data.dateStamp)
-		if err != nil {
-			return false, timestampOfTrade, err
-		}
-
-		timeDiff := time.Now().Sub(timestampOfTrade)
-		timeDiffSeconds := timeDiff.Seconds()
-		timeStampOK = timeDiffSeconds < 5
-	}
-	return timeStampOK, timeStampOfTrade, nil
-}
-
-func strToInt(priceAsString string) (int, error) {
-	intsMap := map[string]int{
-		"0": 0,
-		"1": 1,
-		"2": 2,
-		"3": 3,
-		"4": 4,
-		"5": 5,
-		"6": 6,
-		"7": 7,
-		"8": 8,
-		"9": 9,
-	}
-
-	var strValAsInt int
-
-	for i, v := range priceAsString {
-		val, exists := intsMap[string(v)]
-		if !exists {
-			return 0, errors.New("The price is not a numerical value")
-		}
-		posMultiplier := int(math.Pow10(len(priceAsString) - i - 1))
-		strValAsInt = (posMultiplier * val) + strValAsInt
-	}
-	return strValAsInt, nil
-}
-
-func expectedResults (tradeObjects []string, nowProvider func() time.Time) {
-
 }
