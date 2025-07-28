378477ff521f983f47c420ff20b388954bb5d254
Robt800 <53785367+Robt800@users.noreply.github.com>
2025-07-28 19:22:22 +0100

    small change to remove unnecessary flag within output.go

--- output/output.go
+++ output/output.go
@@ -11,19 +11,17 @@ import (
 
 // Outputs ensures the data from the channel (i.e. the trade) is genuine - if it is, it prints it
 func Outputs(ctx context.Context, producedData chan trades.TradeItems, tradeStore store.TradeStore, alertStore store.AlertDefStore, write io.Writer) error {
-	done := false
-	for !done {
+
+	for {
 		select {
 
 		case <-ctx.Done():
 			fmt.Fprintf(write, "Context cancelled\n")
 			fmt.Fprintf(write, "The error was: %v\n", ctx.Err())
-			done = true
 			return ctx.Err()
 
 		case tradeData, ok := <-producedData:
 			if !ok {
-				done = true
 				return nil
 			}
 			_, _ = fmt.Fprintf(write, "%v\n", tradeData)
@@ -48,7 +46,6 @@ func Outputs(ctx context.Context, producedData chan trades.TradeItems, tradeStor
 			}
 		}
 	}
-	return nil
 }
 
 func tradeMeetsAlertCriteria(tradeData trades.TradeItems, alerts []models.AlertsByItemReturned) bool {
